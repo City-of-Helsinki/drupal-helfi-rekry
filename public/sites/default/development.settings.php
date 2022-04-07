d96e22485bd3c5a4a20c2deebc887ff78af678ea
Risto-Pekka Nykänen <risto.nykanen@druid.fi>
2022-04-07 13:22:05 +0300

    enabled helfi proxy, added env specific settings

---
+++ public/sites/default/development.settings.php
@@ -0,0 +1,10 @@
+<?php
+
+$config['helfi_proxy.settings']['asset_path'] = 'dev-rekry-assets';
+
+$config['helfi_proxy.settings']['prefixes'] = [
+  'en' => 'dev-open-jobs',
+  'fi' => 'dev-avoimet-tyopaikat',
+  'sv' => 'dev-lediga-jobb',
+  'ru' => 'dev-open-jobs',
+];
---
+++ public/sites/default/staging.settings.php
@@ -0,0 +1,10 @@
+<?php
+
+$config['helfi_proxy.settings']['asset_path'] = 'staging-rekry-assets';
+
+$config['helfi_proxy.settings']['prefixes'] = [
+  'en' => 'staging-open-jobs',
+  'fi' => 'staging-avoimet-tyopaikat',
+  'sv' => 'staging-lediga-jobb',
+  'ru' => 'staging-open-jobs',
+];
