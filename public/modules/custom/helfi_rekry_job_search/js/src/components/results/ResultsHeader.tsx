313a5adc822ef033cf40c5a11eccfd609f881fea
Jere Ljungberg <jere@mysteerio.fi>
2022-11-18 11:44:43 +0200

    UHF-5297: Add loading react app

--- public/modules/custom/helfi_rekry_job_search/js/src/components/results/ResultsHeader.tsx
+++ public/modules/custom/helfi_rekry_job_search/js/src/components/results/ResultsHeader.tsx
@@ -3,13 +3,13 @@ import type SearchState from '../../types/SearchState';
 const ResultsHeader = (searchState: SearchState) => {
   const { results } = searchState;
 
-  if (!results) {
+  if (!results || !results?.hits?.total) {
     return null;
   }
 
   const resultString = Drupal.t(
     'vacancies (@announcements announcements)',
-    { '@announcements': results.hits?.total },
+    { '@announcements': results.hits?.total || 0 },
     { context: 'HELfi Rekry job search' }
   );
 
