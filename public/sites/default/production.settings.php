12c66d3da55994a1fe4d911c53412f27f24687ca
Risto-Pekka Nykänen <risto.nykanen@druid.fi>
2023-01-13 09:20:30 +0200

    proxy config

--- public/sites/default/production.settings.php
+++ public/sites/default/production.settings.php
@@ -1,13 +1,4 @@
 <?php
 
-$config['helfi_proxy.settings']['asset_path'] = 'rekry-assets';
-
-$config['helfi_proxy.settings']['prefixes'] = [
-  'en' => 'open-jobs',
-  'fi' => 'avoimet-tyopaikat',
-  'sv' => 'lediga-jobb',
-  'ru' => 'open-jobs',
-];
-
 $config['openid_connect.client.tunnistamo']['settings']['is_production'] = TRUE;
 $config['helfi_proxy.settings']['tunnistamo_return_url'] = '/fi/avoimet-tyopaikat/openid-connect/tunnistamo';
