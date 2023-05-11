af38080ea12e520fa837eb32ac4d2842b78e8768
Jere Ljungberg <jere@mysteerio.fi>
2023-05-11 13:38:03 +0300

    Remove all JS related stuff from custom job search module

--- public/modules/custom/helfi_rekry_job_search/js/src/types/drupalSettings.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace drupalSettings {
-  const path: {
-    currentLanguage: string;
-  };
-  const helfi_rekry_job_search: {
-    elastic_proxy_url: string;
-    results_page_path: string;
-  };
-}
