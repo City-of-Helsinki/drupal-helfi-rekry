7ca1d78a824c2208392c7dcc36508a042378741b
rpnykanen <risto.nykanen@druid.fi>
2023-06-05 14:08:34 +0300

    updated comment

--- public/modules/custom/helfi_rekry_content/src/EventSubscriber/JobImportSubscriber.php
+++ public/modules/custom/helfi_rekry_content/src/EventSubscriber/JobImportSubscriber.php
@@ -10,7 +10,7 @@ use Drupal\migrate\Event\MigratePostRowSaveEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
- * Class for subscribing to job import events.
+ * EventSubscriber for marking newly created job nodes for translation.
  */
 class JobImportSubscriber implements EventSubscriberInterface {
 
