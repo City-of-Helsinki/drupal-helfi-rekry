0cc7bb19b675f35b37137ba834da23638d5be9ab
rpnykanen <risto.nykanen@druid.fi>
2023-09-14 11:11:43 +0300

    UHF-8946: code fixes

--- public/modules/custom/helfi_rekry_content/tests/dtt/src/ExistingSite/JoblistingRedirectTest.php
+++ public/modules/custom/helfi_rekry_content/tests/dtt/src/ExistingSite/JoblistingRedirectTest.php
@@ -21,16 +21,14 @@ class JoblistingRedirectTest extends ExistingSiteBase {
       'type' => 'job_listing',
       'langcode' => 'sv',
       'title' => 'en jobb',
-      'field_recruitment_id' => 'TESTI-1234-56-7890'
+      'field_recruitment_id' => 'TESTI-1234-56-7890',
     ]);
 
     $path = $node->toUrl()->toString();
     $recruitmentId = array_reverse(explode('/', $path))[0];
 
     $this->drupalGet("/fi/avoimet-tyopaikat/avoimet-tyopaikat/$recruitmentId");
-
     $url = $this->getSession()->getCurrentUrl();
-
     $this->assertTrue(str_ends_with($url, '/sv/lediga-jobb/lediga-jobb/testi-1234-56-7890'));
   }
 
