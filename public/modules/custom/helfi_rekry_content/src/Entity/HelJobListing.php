5eb1bf36ab0181d035a46936c529b07c9c5352cc
Tero Elonen <tero.elonen@druid.fi>
2023-10-17 16:33:02 +0300

    UHF-8705: Add bundle class for job listing to remove nested ifs

---
+++ public/modules/custom/helfi_rekry_content/src/Entity/HelJobListing.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Drupal\helfi_rekry_content\Entity;
+
+use Drupal\node\Entity\Node;
+
+/**
+ * Bundle class for hel_map paragraph.
+ */
+class HelJobListing extends Node {
+
+  public function __construct(array $values, $entity_type, $bundle = FALSE, $translations = []) {
+    parent::__construct($values, $entity_type, $bundle, $translations);
+
+    $this->storage = \Drupal::entityTypeManager()
+      ->getStorage('taxonomy_term');
+  }
+
+  /**
+   * Get job description or override value.
+   *
+   * @return string
+   *   Job description.
+   */
+  public function getJobDescription() : string {
+    return $this->get('field_job_description_override')->value ?: $this->get('job_description')->value;
+  }
+
+  /**
+   * Get translated organization name if available or override value.
+   *
+   * @return string
+   *   Organization name.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   * @throws \Drupal\Core\TypedData\Exception\MissingDataException
+   */
+  public function getOrganizationName() : string {
+    if (!$this->get('field_organization_override')->first()) {
+      return $this->get('field_organization_name')->value;
+    }
+
+    $organization_entity = $this->storage->load($this->get('field_organization_override')->first()->target_id);
+
+    if (!$organization_entity->hasTranslation($this->get('langcode')->value)) {
+      return $organization_entity->getName();
+    }
+
+    $translated_organization_entity = $organization_entity->getTranslation($this->get('langcode')->value);
+    return $translated_organization_entity->getName();
+  }
+
+  /**
+   * Get translated employment type if available.
+   *
+   * @return string
+   *   Employment type.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   * @throws \Drupal\Core\TypedData\Exception\MissingDataException
+   */
+  public function getEmploymentType() : string {
+    if (!$this->get('field_employment_type')->first()) {
+      return '';
+    }
+
+    $employment_type_entity = $this->storage->load($this->get('field_employment_type')->first()->target_id);
+
+    if (!$employment_type_entity->hasTranslation($this->get('langcode')->value)) {
+      return $employment_type_entity->getName();
+    }
+
+    $translated_employment_type_entity = $employment_type_entity->getTranslation($this->get('langcode')->value);
+    return $translated_employment_type_entity->getName();
+
+  }
+
+}
