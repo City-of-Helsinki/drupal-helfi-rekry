02f29528934a31b026f6d46bd39ca7aae1077bc0
tuutti <tuutti@iki.fi>
2023-11-30 15:14:01 +0200

    UHF-8943: Refactored job listing to use migration hooks

--- docker/openshift/crons/migrate-changed-job-listings.sh
+++ docker/openshift/crons/migrate-changed-job-listings.sh
@@ -1,71 +1,16 @@
 #!/bin/bash
 
 echo "Starting job listing:changed migrations: $(date)"
-
-function populate_variables {
-  # Generate variables used to control which migrates needs
-  # to be reset and which ones needs to be skipped based on
-  # migrate status
-  MIGRATE_STATUS=$(drush migrate:status --format=json)
-  php ./docker/openshift/crons/migrate-status.php \
-    helfi_rekry_jobs:changed,helfi_rekry_jobs:changed_sv,helfi_rekry_jobs:changed_en \
-    "$MIGRATE_STATUS" > /tmp/migrate-job-listings-source.sh \
-    $1
-
-  # Contains variables:
-  # - $RESET_STATUS
-  # - $SKIP_MIGRATE
-  # Both contains a space separated list of migrates
-  source /tmp/migrate-job-listings-source.sh
-}
-
-function reset_status {
-  # Reset status of stuck migrations.
-  for ID in $RESET_STATUS; do
-    drush migrate:reset-status $ID
-  done
-}
-
-function run_migrate {
-  for ID in $SKIP_MIGRATE; do
-    if [ "$ID" == "$1" ]; then
-      return 1
-    fi
-  done
-  return 0
-}
-
-# Populate variables for the first run after deploy and
-# default migrate interval to 6 hours.
-populate_variables 21600
-
 while true
 do
-  # Reset stuck migrations.
-  reset_status
-
+  # Run migrations every 15 minutes, reset stuck migrations every 12 hours.
   echo "Running job listing:changed migrations: $(date)"
-  if run_migrate "helfi_rekry_jobs:changed"; then
-    echo "Running job listing:changed migrations: $(date)"
-    drush migrate:import helfi_rekry_jobs:changed --update
-  fi
+  drush migrate:import helfi_rekry_jobs:changed --update --reset-threshold 43200 --interval 900
+  echo "Running job listing:changed migrations (sv): $(date)"
+  drush migrate:import helfi_rekry_jobs:changed_sv --update --reset-threshold 43200 --interval 900
+  echo "Running job listing:changed migrations (en): $(date)"
+  drush migrate:import helfi_rekry_jobs:changed_en --update --reset-threshold 43200 --interval 900
 
-  if run_migrate "helfi_rekry_jobs:changed_sv"; then
-    echo "Running job listing:changed migrations (sv): $(date)"
-    drush migrate:import helfi_rekry_jobs:changed_sv --update
-  fi
-
-  if run_migrate "helfi_rekry_jobs:changed_en"; then
-    echo "Running job listing:changed migrations (en): $(date)"
-    drush migrate:import helfi_rekry_jobs:changed_en --update
-  fi
-
-  # Reset migrate status if migrate has been running for more
-  # than 12 hours.
-  populate_variables 43200
-  # Never skip migrate after first time.
-  SKIP_MIGRATE=
-
-  # Sleep for ½ hour.
+  # Sleep for 30 minutes.
   sleep 1800
 done
