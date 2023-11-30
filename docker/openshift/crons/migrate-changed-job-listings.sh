3d0982fe3f85fb610df30459f91d316be82ea656
tuutti <tuutti@iki.fi>
2023-11-30 15:27:56 +0200

    UHF-8943: Refactored job-listing migrate too

--- docker/openshift/crons/migrate-changed-job-listings.sh
+++ docker/openshift/crons/migrate-changed-job-listings.sh
@@ -3,12 +3,10 @@
 echo "Starting job listing:changed migrations: $(date)"
 while true
 do
-  # Run migrations every 15 minutes, reset stuck migrations every 12 hours.
   echo "Running job listing:changed migrations: $(date)"
+  # Allow migrations to be run every 15 minutes, reset stuck migrations every 12 hours.
   drush migrate:import helfi_rekry_jobs:changed --update --reset-threshold 43200 --interval 900
-  echo "Running job listing:changed migrations (sv): $(date)"
   drush migrate:import helfi_rekry_jobs:changed_sv --update --reset-threshold 43200 --interval 900
-  echo "Running job listing:changed migrations (en): $(date)"
   drush migrate:import helfi_rekry_jobs:changed_en --update --reset-threshold 43200 --interval 900
 
   # Sleep for 30 minutes.
