eb13978b9f2cdd3464c3ef32215b9857ae5cf199
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2024-02-01 12:48:39 +0200

    UHF-9587: Make logs more readable
    Add --no-progress option to all job listing migrations.

--- docker/openshift/crons/migrate-job-listings.sh
+++ docker/openshift/crons/migrate-job-listings.sh
@@ -5,17 +5,17 @@ while true
 do
   echo "Running job listing migrations: $(date)"
   # Allow migrations to be run every 1.5 hours, reset stuck migrations every 12 hours.
-  drush migrate:import helfi_rekry_images:all --reset-threshold 43200 --interval 5400
-  drush migrate:import helfi_rekry_videos:all --reset-threshold 43200 --interval 5400
-  drush migrate:import helfi_rekry_task_areas:all --reset-threshold 43200 --interval 5400
-  drush migrate:import helfi_rekry_task_areas:all_sv --reset-threshold 43200 --interval 5400
-  drush migrate:import helfi_rekry_task_areas:all_en --reset-threshold 43200 --interval 5400
-  drush migrate:import helfi_rekry_organizations:all --update --reset-threshold 43200 --interval 5400
-  drush migrate:import helfi_rekry_organizations:all_sv --update --reset-threshold 43200 --interval 5400
-  drush migrate:import helfi_rekry_organizations:all_en --update --reset-threshold 43200 --interval 5400
-  drush migrate:import helfi_rekry_jobs:all --reset-threshold 43200 --interval 5400
-  drush migrate:import helfi_rekry_jobs:all_sv --reset-threshold 43200 --interval 5400
-  drush migrate:import helfi_rekry_jobs:all_en --reset-threshold 43200 --interval 5400
+  drush migrate:import helfi_rekry_images:all --reset-threshold 43200 --interval 5400 --no-progress
+  drush migrate:import helfi_rekry_videos:all --reset-threshold 43200 --interval 5400 --no-progress
+  drush migrate:import helfi_rekry_task_areas:all --reset-threshold 43200 --interval 5400 --no-progress
+  drush migrate:import helfi_rekry_task_areas:all_sv --reset-threshold 43200 --interval 5400 --no-progress
+  drush migrate:import helfi_rekry_task_areas:all_en --reset-threshold 43200 --interval 5400 --no-progress
+  drush migrate:import helfi_rekry_organizations:all --update --reset-threshold 43200 --interval 5400 --no-progress
+  drush migrate:import helfi_rekry_organizations:all_sv --update --reset-threshold 43200 --interval 5400 --no-progress
+  drush migrate:import helfi_rekry_organizations:all_en --update --reset-threshold 43200 --interval 5400 --no-progress
+  drush migrate:import helfi_rekry_jobs:all --reset-threshold 43200 --interval 5400 --no-progress
+  drush migrate:import helfi_rekry_jobs:all_sv --reset-threshold 43200 --interval 5400 --no-progress
+  drush migrate:import helfi_rekry_jobs:all_en --reset-threshold 43200 --interval 5400 --no-progress
 
   drush helfi-rekry-content:clean-expired-listings
 
