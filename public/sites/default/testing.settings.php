b1dd2be25745c06334befa3d4d7b78f0186eb5d7
tuutti <tuutti@iki.fi>
2024-02-13 11:11:13 +0200

    UHF-9546: Removed unnecessary settings

--- public/sites/default/testing.settings.php
+++ public/sites/default/testing.settings.php
@@ -1,12 +1,3 @@
 <?php
 
 $config['helfi_proxy.settings']['tunnistamo_return_url'] = '/fi/avoimet-tyopaikat/openid-connect/tunnistamo';
-$config['helfi_proxy.settings']['asset_path'] = 'rekry-assets';
-
-$config['helfi_proxy.settings']['prefixes'] = [
-  'en' => 'open-jobs',
-  'fi' => 'avoimet-tyopaikat',
-  'sv' => 'lediga-jobb',
-  'ru' => 'open-jobs',
-  'zxx' => 'open-jobs',
-];
