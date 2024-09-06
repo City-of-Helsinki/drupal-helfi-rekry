0f75547d82eaa133af9285f35127066d098441b6
rpnykanen <risto.nykanen@druid.fi>
2024-09-06 09:12:21 +0300

    UHF-8706: override the key from env variables

--- public/sites/default/production.settings.php
+++ public/sites/default/production.settings.php
@@ -2,3 +2,4 @@
 
 $config['openid_connect.client.tunnistamo']['settings']['is_production'] = TRUE;
 $config['helfi_proxy.settings']['tunnistamo_return_url'] = '/fi/avoimet-tyopaikat/openid-connect/tunnistamo';
+$config['helfi_google_api.settings']['indexing_api_key'] = getenv('GOOGLE-INDEXING-API-KEY');
