d4f255807d9580e2c66a22d2aac50dfedc2a749b
rpnykanen <risto.nykanen@druid.fi>
2024-09-10 18:31:48 +0300

    UHF-8706: added factory and updated services

---
+++ public/modules/custom/helfi_google_api/src/GoogleServiceFactory.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\helfi_google_api;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Google\Client as GoogleClient;
+use Google\Service\Indexing;
+
+/**
+ * Create and set up Google service and client.
+ */
+class GoogleServiceFactory {
+
+  /**
+   * Api scopes.
+   */
+  const SCOPES = ['https://www.googleapis.com/auth/indexing'];
+
+  /**
+   * Google service factory method.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
+   *   The config factory.
+   *
+   * @return \Google\Service\Indexing
+   *   Google indexing service.
+   */
+  public function create(ConfigFactoryInterface $configFactory): Indexing {
+    $config = $configFactory->get('helfi_google_api.settings');
+    $key = $config->get('indexing_api_key') ?: '';
+
+    $client = new GoogleClient();
+    $client->setApplicationName('Helfi_Rekry');
+    $client->addScope(self::SCOPES);
+    $client->setUseBatch(TRUE);
+
+    // Set empty json object as a key if necessary.
+    if ($key) {
+      $client->setAuthConfig(json_decode($key, TRUE));
+      $client->authorize();
+    }
+
+    return new Indexing($client);
+  }
+
+}
