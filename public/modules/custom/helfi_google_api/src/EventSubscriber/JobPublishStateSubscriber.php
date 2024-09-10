c5ebd3d32f6903a2662be48950dd6a234eb062dc
rpnykanen <risto.nykanen@druid.fi>
2024-09-10 18:34:50 +0300

    UHF-8706: get rid of the simple prod check

--- public/modules/custom/helfi_google_api/src/EventSubscriber/JobPublishStateSubscriber.php
+++ public/modules/custom/helfi_google_api/src/EventSubscriber/JobPublishStateSubscriber.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Drupal\helfi_google_api\EventSubscriber;
 
-use Drupal\helfi_api_base\Environment\EnvironmentEnum;
-use Drupal\helfi_api_base\Environment\EnvironmentResolverInterface;
 use Drupal\helfi_google_api\JobIndexingService;
 use Drupal\helfi_rekry_content\Entity\JobListing;
 use Drupal\scheduler\SchedulerEvent;
@@ -21,12 +19,9 @@ class JobPublishStateSubscriber implements EventSubscriberInterface {
    *
    * @param \Drupal\helfi_google_api\JobIndexingService $jobIndexingService
    *   The job indexing service.
-   * @param \Drupal\helfi_api_base\Environment\EnvironmentResolverInterface $environmentResolver
-   *   The environment resolver.
    */
   public function __construct(
     private readonly JobIndexingService $jobIndexingService,
-    private readonly EnvironmentResolverInterface $environmentResolver,
   ) {
   }
 
@@ -52,10 +47,6 @@ class JobPublishStateSubscriber implements EventSubscriberInterface {
    *   The scheduler event.
    */
   public function sendIndexingRequest(SchedulerEvent $event): void {
-    if (!$this->isProduction()) {
-      return;
-    }
-
     $entity = $event->getNode();
     if (!$entity instanceof JobListing) {
       return;
@@ -76,10 +67,6 @@ class JobPublishStateSubscriber implements EventSubscriberInterface {
    *   The scheduler event.
    */
   public function sendDeindexingRequest(SchedulerEvent $event): void {
-    if (!$this->isProduction()) {
-      return;
-    }
-
     $entity = $event->getNode();
     if (!$entity instanceof JobListing) {
       return;
@@ -91,16 +78,6 @@ class JobPublishStateSubscriber implements EventSubscriberInterface {
     catch (\Exception) {
       // Has been logged by indexing service.
     }
-  }
-
-  /**
-   * Check if in production.
-   *
-   * @return bool
-   *   Is production.
-   */
-  private function isProduction(): bool {
-    return $this->environmentResolver->getActiveEnvironment()->getEnvironment() === EnvironmentEnum::Prod;
   }
 
 }
