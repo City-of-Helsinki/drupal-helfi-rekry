bc76cf9c2b8fc7f12d49ca52b0e1c8d055d905ec
rpnykanen <risto.nykanen@druid.fi>
2024-10-09 08:47:04 +0300

    UHF-10581: disable until tested in prod

--- public/sites/default/production.settings.php
+++ public/sites/default/production.settings.php
@@ -5,4 +5,4 @@ $config['helfi_proxy.settings']['tunnistamo_return_url'] = '/fi/avoimet-tyopaika
 $config['helfi_google_api.settings']['indexing_api_key'] = getenv('GOOGLE_INDEXING_API_KEY');
 
 // Remove the comment when it's time to enable the feature on production
-$config['helfi_google_api.settings']['dry_run'] = FALSE;
+// $config['helfi_google_api.settings']['dry_run'] = FALSE;
