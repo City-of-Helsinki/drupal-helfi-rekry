9ef4cf7e0b170d4d0f35d4253e00d4282b8e20a6
rpnykanen <risto.nykanen@druid.fi>
2024-10-22 15:04:39 +0300

    better comment

--- public/sites/default/production.settings.php
+++ public/sites/default/production.settings.php
@@ -4,5 +4,5 @@ $config['openid_connect.client.tunnistamo']['settings']['is_production'] = TRUE;
 $config['helfi_proxy.settings']['tunnistamo_return_url'] = '/fi/avoimet-tyopaikat/openid-connect/tunnistamo';
 $config['helfi_google_api.settings']['indexing_api_key'] = getenv('GOOGLE_INDEXING_API_KEY');
 
-// Remove the comment when it's time to enable the feature on production
+// Sends indexing requests to google when dry_run is set to false.
 $config['helfi_google_api.settings']['dry_run'] = FALSE;
