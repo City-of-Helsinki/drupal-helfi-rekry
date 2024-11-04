ce5536537ecea86ea98ea8ef51ec92b51e0ba2e7
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2024-11-04 08:50:37 +0200

    UHF-10719: Fix PrepareIndex

---
+++ public/modules/custom/helfi_rekry_content/src/EventSubscriber/ElasticsearchEventSubscriber.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\helfi_rekry_content\EventSubscriber;
+
+use Drupal\Component\Utility\NestedArray;
+use Drupal\elasticsearch_connector\Event\AlterSettingsEvent;
+use Drupal\elasticsearch_connector\Event\IndexParamsEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * {@inheritdoc}
+ */
+class ElasticsearchEventSubscriber implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents(): array {
+    return [
+      AlterSettingsEvent::class => 'prepareIndices',
+    ];
+  }
+
+  /**
+   * Method to prepare index.
+   *
+   * @param \Drupal\elasticsearch_connector\Event\AlterSettingsEvent $event
+   *   The PrepareIndex event.
+   */
+  public function prepareIndices(AlterSettingsEvent $event): void {
+    $indexName = $event->getIndex()->id();
+    $finnishIndices = [
+      'job_listings',
+    ];
+    if (in_array($indexName, $finnishIndices)) {
+      $event->setSettings(NestedArray::mergeDeep(
+        $event->getSettings(),
+        [
+          'analysis' => [
+            'analyzer' => [
+              'default' => [
+                'type' => 'finnish',
+              ],
+            ],
+          ],
+        ],
+      ));
+    }
+  }
+
+}
