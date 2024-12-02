d21ce7efa98f24c36daa803310d7441a6730ecb0
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2024-12-02 15:15:29 +0200

    UHF-9937: Rename scripts

--- docker/openshift/crons/migrate-job-listings.sh
+++ docker/openshift/crons/migrate-job-listings.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
 migrations=(
-  "helfi_rekry_images:all"
-  "helfi_rekry_videos:all"
-  "helfi_rekry_task_areas:fi"
-  "helfi_rekry_task_areas:sv"
-  "helfi_rekry_task_areas:en"
-  "helfi_rekry_organizations:fi"
-  "helfi_rekry_organizations:sv"
-  "helfi_rekry_organizations:en"
-  "helfi_rekry_employments:all"
-  "helfi_rekry_employment_types:all"
+  "helfi_rekry_jobs:all"
 )
 
 echo "Starting job listing migrations: $(date)"
@@ -19,12 +10,10 @@ do
   echo "Running job listing migrations: $(date)"
 
   for migration in "${migrations[@]}"; do
-    # Allow migrations to be run every 1.5 hours, reset stuck migrations every 12 hours.
-    drush migrate:import "$migration" --reset-threshold 43200 --interval 5400 --no-progress
+    # Allow migrations to be run every 10 minutes, reset stuck migrations every 30 minutes.
+    drush migrate:import "$migration" --reset-threshold 1800 --interval 600 --no-progress
   done
 
-  drush helfi-rekry-content:clean-expired-listings
-
-  # Sleep for 3 hours.
-  sleep 10800
+  # Sleep for 15 minutes.
+  sleep 900
 done
